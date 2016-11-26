86dc4f477a84a65ddfd927881e261dd3b4bb4594
awkwardusername <awkwardusername@gmail.com>
2016-11-26 23:14:32 +0800

    add app config file

--- config/ggpay.php
+++ config/ggpay.php
@@ -1,1 +1,23 @@
 <?php
+
+return [
+    'accounts' => [
+        'source' => [
+            'account_no' => env('ACCOUNT_OMEGA')
+        ]
+    ],
+
+    'apis' => [
+        'chikka'  => [
+            'base_url'   => 'https://post.chikka.com/smsapi/request',
+            'client_id'  => env('CHIKKA_CLIENT_ID'),
+            'secret_key' => env('CHIKKA_SECRET_KEY'),
+            'short_code' => env('CHIKKA_SHORT_CODE')
+        ],
+        'paymaya' => [
+            'base_url' => 'https://pg-sandbox.paymaya.com/payments/v1',
+            'public'   => env('PAYMAYA_PUBLIC'),
+            'secret'   => env('PAYMAYA_SECRET')
+        ]
+    ]
+];
